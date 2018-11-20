aa93179623985da8c27961d385cf712dced959d0
charleneccc <g1165973@study.ouhk.edu.hk>
2018-11-20 20:45:37 +0800

    - change player picture

--- runnersHigh-master/app/src/main/java/com/alarmclocksnoozers/runnershigh/Player.java
+++ runnersHigh-master/app/src/main/java/com/alarmclocksnoozers/runnershigh/Player.java
@@ -56,7 +56,7 @@ public class Player{
 		speedoffsetXMax = Util.getPercentOfScreenWidth(7);
 		speedoffsetXStep = Util.getPercentOfScreenWidth(0.002f);
 		
-		playerSpriteImg = Util.loadBitmapFromAssets("game_character_spritesheet.png");
+		playerSpriteImg = Util.loadBitmapFromAssets("stickerman.png");
 		playerSprite = new Sprite(x, y, 0.5f, width, height, 25, 8); 
 		playerSprite.loadBitmap(playerSpriteImg); 
 		glrenderer.addMesh(playerSprite);
